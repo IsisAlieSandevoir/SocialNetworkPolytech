2d971817b37f6e7bae1822a293d584277bc842c9
isis <isis.alie@hotmail.fr>
2018-04-11 08:55:05 +0200

    End of SQL request and appConfig

--- src/main/java/com/polytech/persistence/JdbcRepository.java
+++ src/main/java/com/polytech/persistence/JdbcRepository.java
@@ -2,17 +2,47 @@ package com.polytech.persistence;
 
 import com.polytech.services.Story;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcRepository implements StoryRepository{
 
+    private Connection connection;
+
+    public JdbcRepository(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public void save(Story story) {
-
+        String query = "INSERT INTO STORY (CONTENT) VALUES('"+story.getContent()+"')";
+        try {
+            Statement statement  = connection.createStatement();
+            statement.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Story> fetch() {
-        return null;
+        String query = "SELECT * FROM STORY ";
+        List<Story> stories = new ArrayList<>();
+        try {
+            Statement statement  = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while(resultSet.next()) {
+                String content = resultSet.getString("CONTENT");
+                stories.add(new Story(content));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return stories;
     }
 }
--- src/test/java/PublicationTest.java
+++ src/test/java/PublicationTest.java
@@ -1,13 +1,14 @@
+import com.polytech.Config.AppConfig;
 import com.polytech.persistence.InMemoryRepository;
 import com.polytech.persistence.StoryRepository;
 import com.polytech.services.FeedService;
 import com.polytech.services.PublicationService;
 import com.polytech.services.Story;
 import com.polytech.web.FeedController;
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,10 +19,11 @@ public class PublicationTest {
 
     @Before
     public void setUp() {
-        StoryRepository storyRepository = new InMemoryRepository();
-        PublicationService publicationService = new PublicationService(storyRepository);
-        FeedService feedService = new FeedService(storyRepository);
-        feedController = new FeedController(publicationService, feedService);
+        //StoryRepository storyRepository = new InMemoryRepository();
+        //PublicationService publicationService = new PublicationService(storyRepository);
+        //FeedService feedService = new FeedService(storyRepository);
+        AnnotationConfigApplicationContext container = new AnnotationConfigApplicationContext(AppConfig.class);
+        feedController = container.getBean(FeedController.class);
     }
 
     @Test
